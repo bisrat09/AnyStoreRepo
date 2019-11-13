ea8068d6fa9038d0e3a9876e8dc496f87ea9ab74
bisrat <bisrat09@gmail.com>
2019-11-13 13:26:31 -0800

    designed a User form for the User module

--- UI/frmUsers.cs
+++ UI/frmUsers.cs
@@ -26,5 +26,40 @@ namespace AnyStore.UI
         {
 
         }
+
+        private void Label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Label1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void TextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void TextBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Label1_Click_2(object sender, EventArgs e)
+        {
+
+        }
+
+        private void FrmUsers_Load(object sender, EventArgs e)
+        {
+
+        }
     }
 }
