7153e2c6addf7c9c6e6947c61fe06211c0f6dc4d
bisrat <bisrat09@gmail.com>
2019-12-04 15:29:11 -0800

    added fuctionality to display Categories from database into combobox

--- UI/frmProducts.cs
+++ UI/frmProducts.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using AnyStore.DAL;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -20,6 +21,21 @@ namespace AnyStore.UI
         private void pictureBoxClose_Click(object sender, EventArgs e)
         {
             this.Hide();
+        }
+
+        categoriesDAL cDal = new categoriesDAL();
+
+        private void frmProducts_Load(object sender, EventArgs e)
+        {
+            // create datatable to hold the categories from Db 
+            DataTable categoriesDT = cDal.Select();
+
+            // specifify data source for category Combobox
+            cmbCategory.DataSource = categoriesDT;
+
+            //specify display  member and value member for combobox
+            cmbCategory.DisplayMember = "title";
+            cmbCategory.ValueMember = "title";
         }
     }
 }
